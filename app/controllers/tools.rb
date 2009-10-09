ae428278d9370c348ff2eb6c91bf7e6f8f38448d
Andre Ben Hamou <andre@bluetheta.com>
2009-10-09 12:27:11 +0100

    Fixed unassociated list in tools/icons.

--- app/controllers/tools.rb
+++ app/controllers/tools.rb
@@ -23,8 +23,8 @@ class Tools < Application
     @properties = PropertyDefinition.all.sort_by { |property| property.sequence_number }
     @icon_urls_by_property_id = PropertyDefinition.icon_urls_by_property_id(@properties)
     
-    used_names = @icon_urls_by_property_id.values.map { |url| url.split("/").last }
-    @unused_property_icons = Asset.all(:bucket => "property_icons", :name.not => used_names).sort_by { |a| a.name }
+    used_checksums = @icon_urls_by_property_id.values.map { |url| File.basename(url.split("/").last, ".png") }
+    @unused_property_icons = Asset.all(:bucket => "property_icons", :checksum.not => used_checksums).sort_by { |a| a.name }
     
     @skip_javascript = true
     render

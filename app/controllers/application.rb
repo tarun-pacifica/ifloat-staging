58e47401b6fa46366cac9367c39e2fa786bfe899
Andre Ben Hamou <andre@bluetheta.com>
2010-01-24 16:07:05 +0000

    Added terms PDF and allowed for unauthenticated purchase tracking.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,6 +2,6 @@ class Application < Merb::Controller
   before :ensure_authenticated, :exclude => [:login]
   
   def ensure_authenticated
-    redirect "/prelaunch/login" unless Merb.environment == "development" or session.authenticated?
+    redirect "/prelaunch/login" unless params[:action] == "track" or Merb.environment == "development" or session.authenticated?
   end
 end

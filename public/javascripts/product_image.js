4f4fd2095f9b632442ee26f1171e40f6f88fd92d
Andre Ben Hamou <andre@bluetheta.com>
2010-03-17 10:34:45 +0000

    Tidied up product_image.js.

--- public/javascripts/product_image.js
+++ public/javascripts/product_image.js
@@ -5,17 +5,17 @@ function product_image_make(url, popup_url) {
 
 function product_image_popup(event, image_url) {
 	var zoom = $("#image_popup");
-	zoom[0].src = image_url;
+	zoom.attr('src', image_url);
 	
 	var image = $(event.target)
 	var position = image.offset();
-	image.css("border-color", "black");
-	zoom.css("left", position.left - 10 - zoom.width() + "px");
-	zoom.css("top", position.top + (image.height() - zoom.height()) / 2 + "px");
-	zoom.css("display", "block");
+	image.css('border-color', 'black');
+	zoom.css('left', position.left - 10 - zoom.width() + 'px');
+	zoom.css('top', position.top + (image.height() - zoom.height()) / 2 + 'px');
+	zoom.css('display', 'block');
 }
 
 function prod_image_unpopup(i) {
-	$(i).css("border-color", "gray");
-	$("#image_popup").css("display", "none");
+	$(i).css('border-color', 'gray');
+	$('#image_popup').css('display', 'none');
 }

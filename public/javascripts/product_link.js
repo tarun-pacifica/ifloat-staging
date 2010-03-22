a70f916d6657d2b945adc381ef752abede8152dc
Andre Ben Hamou <andre@bluetheta.com>
2010-03-22 00:59:34 +0000

    Implemented caching strategy in product link loader.

--- public/javascripts/product_link.js
+++ public/javascripts/product_link.js
@@ -8,14 +8,29 @@ function product_link(product) {
 	return '<a class="product" href="/products/' + product.id + '">' + product_image_make(image_urls[0], image_urls[1]) + title_lines.join(" ") + '<p>' + product.summary + '</p> <hr class="terminator" /> </a>';
 }
 
+var $ifloat_product_links_cache = {};
+
 function product_links_load(product_ids) {
-	var url = '/products/batch/' + product_ids.join('_');
-	$.getJSON(url, product_links_load_handle);	
+	$ifloat_product_links_requested = product_ids;
+	
+	var missing = [];
+	for(i in product_ids) {
+		var id = product_ids[i];
+		if($ifloat_product_links_cache[id] == undefined) missing.push(id);
+	}
+	
+	if(missing.length == 0) product_links_load_handle([]);
+	else $.getJSON('/products/batch/' + missing.join('_'), product_links_load_handle);	
 }
 
 function product_links_load_handle(products) {
 	for(i in products) {
 		var product = products[i];
-		$('#prod_' + product.id).html(product_link(product));
+		$ifloat_product_links_cache[product.id] = product;
+	}
+	
+	for(i in $ifloat_product_links_requested) {
+		var id = $ifloat_product_links_requested[i];
+		$('#prod_' + id).html(product_link($ifloat_product_links_cache[id]));
 	}
 }

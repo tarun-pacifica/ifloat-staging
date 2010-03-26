815157a5eca918a320646e87e7381305d1c4c220
Andre Ben Hamou <andre@bluetheta.com>
2010-03-26 16:27:49 +0000

    Simplified / improved tooltip.

--- public/javascripts/tooltip.js
+++ public/javascripts/tooltip.js
@@ -2,39 +2,29 @@ function tooltip_hide() {
 	$('#tooltip').css('display', 'none');
 }
 
-function tooltip_preload_backgrounds() {
-	var vert = ["top", "middle", "bottom"];
-	var horiz = ["left", "center", "right"];
-	for(var v in vert) {
-		for(var h in horiz) {
-			util_preload_image("/images/tooltip/backgrounds/" + vert[v] + "_" + horiz[h] + ".png");
-		}
-	}
-}
-
 function tooltip_show(event, text, relative_position) {
-	var bubble = $('#tooltip');
-	bubble.find('p').html(text);
+	var tooltip = $('#tooltip');
+	tooltip.html(text);
 	
 	var target = $(event.target);
 	var position = target.offset();
 	var left, top;
 	
 	if(relative_position == 'above') {
-		left = position.left + ((target.width() - bubble.width()) / 2);
-		top = position.top - bubble.height() - 3;
+		left = position.left + ((target.width() - tooltip.outerWidth()) / 2);
+		top = position.top - tooltip.outerHeight() - 3;
 	} else if(relative_position == 'bottom') {
-		left = position.left + ((target.width() - bubble.width()) / 2);
+		left = position.left + ((target.width() - tooltip.outerWidth()) / 2);
 		top = position.bottom + 3;
 	} else if(relative_position == 'left') {
-		left = position.left - bubble.width() - 3;
-		top = position.top + ((target.height() - bubble.height()) / 2) + 2;
+		left = position.left - tooltip.outerWidth() - 3;
+		top = position.top + ((target.height() - tooltip.outerHeight()) / 2) + 2;
 	} else { // right
 		left = position.left + target.width() + 3;
-		top = position.top + ((target.height() - bubble.height()) / 2) + 2;
+		top = position.top + ((target.height() - tooltip.outerHeight()) / 2) + 2;
 	}
 	
-	bubble.css('left', left + 'px');
-	bubble.css('top', top + 'px');
-	bubble.css('display', 'block');
+	tooltip.css('left', left + 'px');
+	tooltip.css('top', top + 'px');
+	tooltip.fadeIn('fast');
 }

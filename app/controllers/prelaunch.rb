b03489c735a76682efa669159adad348359596fc
Andre Ben Hamou <andre@bluetheta.com>
2010-04-03 14:07:42 +0100

    Retaining prelaunch protections for staging but will get rid of @hide_title support.

--- app/controllers/prelaunch.rb
+++ app/controllers/prelaunch.rb
@@ -1,5 +1,5 @@
 class Prelaunch < Application
-  # TODO: remove for launch (and get rid of @hide_title support in application.html.erb)
+  # TODO: get rid of @hide_title support in application.html.erb
   def login
     redirect("/") if session.authenticated?
     @hide_title = true

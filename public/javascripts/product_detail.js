455263f362f8e4964e9562735f57cd98e75c15b2
Andre Ben Hamou <andre@bluetheta.com>
2010-04-18 18:49:33 +0100

    New pick buttons now in place and wired up.

--- public/javascripts/product_detail.js
+++ public/javascripts/product_detail.js
@@ -4,29 +4,37 @@ function product_detail_more_relations(event) {
 	more.hide();
 }
 
-function product_detail_pick_button_click(action, from_group, to_group, pick_id) {
-	if(action == 'add') pick_list_add(to_group, $ifloat_body.product_id);
-	else if(action == 'move') pick_list_move(from_group, to_group, pick_id);
-	else pick_list_remove(to_group, pick_id);
+function product_detail_pick_button_click(event) {
+	var button = $(event.target);
+	var to_group = null;
+	var groups = ['buy_later', 'buy_now', 'compare'];
+	for(var i in groups) {
+		var g = groups[i];
+		if(button.hasClass(g)) {
+			to_group = g;
+			break;
+		}
+	}
+	
+	var data = button.parent().data();
+	var from_group = data.group;
+	var pick_id = data.pick_id;
+	
+	if(to_group == null) pick_list_remove(to_group, pick_id);
+	else if(from_group) pick_list_move(from_group, to_group, pick_id);
+	else pick_list_add(to_group, $ifloat_body.product_id);
 }
 
 function product_detail_pick_buttons_update(group, pick_id) {
-	return;
 	var pick_buttons = $('#pick_buttons');
-	
 	if(pick_buttons.length == 0) return;
 	
-	var groups = ['compare', 'buy_later', 'buy_now'];
-	var lists = {compare: 'Differentiate', buy_later: 'Future Buys', buy_now: 'Buy Now'};
+	pick_buttons.removeData();
+	if(group) pick_buttons.data('group', group)
+	if(pick_id) pick_buttons.data('pick_id', pick_id);
 	
-	pick_buttons.empty();
-	
-	for(var i in groups) {
-		var g = groups[i];
-		var action = (group ? (group == g ? 'remove' : 'move') : 'add');
-		var click = "product_detail_pick_button_click('" + action + "', '" + group + "', '" + g + "', " + pick_id + ")";
-		pick_buttons.append('<div class="button ' + action + '" onclick="' + click + '">' + lists[g] + '</div>');
-	}
+	pick_buttons.find('.button').removeClass('selected').unbind('click').click(product_detail_pick_button_click);
+	if(group) pick_buttons.find('.' + group).addClass('selected').unbind('click');
 }
 
 function product_detail_select_image(event) {

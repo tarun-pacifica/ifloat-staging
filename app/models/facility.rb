89c414a877346f464fef20527b4fefa19a290fbe
Andre Ben Hamou <andre@bluetheta.com>
2010-05-11 21:57:58 +0100

    Fixed the facility importer to throw away non-unicode characters.

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -75,7 +75,7 @@ class Facility
       product.currency = "GBP"
       
       [:title, :image_url, :description].each do |a|
-        old_val, new_val = product.attribute_get(a), info[a]
+        old_val, new_val = product.attribute_get(a), info[a].unpack("C*").pack("U*")
         product.attribute_set(a, new_val)
         reports << [ref, "updated: #{a}", "from #{old_val.inspect}", "to #{new_val.inspect}"] unless old_val == new_val
       end

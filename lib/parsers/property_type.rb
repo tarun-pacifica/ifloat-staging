8fe54a93d02b6b557222f8c4d57e2a6c4e4199d9
Andre Ben Hamou <andre@bluetheta.com>
2010-06-13 12:49:00 +0100

    Added support for an extra unit column in the PT parser.

--- lib/parsers/property_type.rb
+++ lib/parsers/property_type.rb
@@ -1,5 +1,5 @@
 class PropertyTypeParser < AbstractParser
-  HEADERS = ["property type", "raw type", "unit-1", "unit-2", "unit-3", "unit-4"]
+  HEADERS = ["property type", "raw type", "unit-1", "unit-2", "unit-3", "unit-4", "unit-5"]
   REQUIRED_VALUE_HEADERS = ["property type", "raw type"].to_set
   
   
@@ -7,7 +7,7 @@ class PropertyTypeParser < AbstractParser
   
   def generate_objects(parsed_fields)
     name, core_type = parsed_fields.values_at("property type", "raw type")
-    units = parsed_fields.values_at("unit-1", "unit-2", "unit-3", "unit-4").compact
+    units = parsed_fields.values_at("unit-1", "unit-2", "unit-3", "unit-4", "unit-5").compact
     [ImportObject.new(PropertyType, :name => name, :core_type => core_type, :units => units)]
   end
 end

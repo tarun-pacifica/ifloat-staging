07e04e4fb00f926fbfd6134bb4c54c8402f35596
Andre Ben Hamou <andre@bluetheta.com>
2010-06-27 21:53:56 +0100

    Fixed tools/icons to display blank _required_ property icons with red text.

--- app/controllers/tools.rb
+++ app/controllers/tools.rb
@@ -3,6 +3,8 @@ class Tools < Application
   
   def icons
     @properties = Indexer.property_display_cache.values.sort_by { |info| info[:seq_num] }
+    @blank_icon_url = Asset.first(:bucket => "property_icons", :name => "blank.png").url
+    
     used_checksums = @properties.map { |info| File.basename(info[:icon_url].split("/").last, ".png") }
     @unused_icons = Asset.all(:bucket => "property_icons", :checksum.not => used_checksums).sort_by { |a| a.name }
     

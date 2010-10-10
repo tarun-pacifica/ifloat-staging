e38fc7f9bbd26bb1f9e0a5b1f9b6882c652f86df
Andre Ben Hamou <andre@bluetheta.com>
2010-10-10 21:45:09 +0100

    Prelaunch tidy-up.

--- app/controllers/prelaunch.rb
+++ app/controllers/prelaunch.rb
@@ -1,7 +1,5 @@
 class Prelaunch < Application
-  # TODO: get rid of @hide_title support in application.html.erb
   def login
-    return redirect("/") if session.authenticated?
-    render
+    session.authenticated? ? redirect("/") : render
   end
 end

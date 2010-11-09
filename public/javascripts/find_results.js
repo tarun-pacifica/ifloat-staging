7a4caf5990ef10e516d67f8130391078976edf95
Andre Ben Hamou <andre@bluetheta.com>
2010-11-09 14:54:11 +0000

    Added an add-filter prompt.

--- public/javascripts/find_results.js
+++ public/javascripts/find_results.js
@@ -1,35 +1,43 @@
 function find_results_make(checksum, count, image_url, popup_image_url, titles) {
-	var url = '/cached_finds/' + $ifloat_body.find_id + '/products_for/' + checksum;
-	var tally = '<div class="tally">' + count + ' item' + (count > 1 ? 's' : '') + '</div>';
-	return '<a class="product" href="' + url + '"> ' + tally + product_image_make(image_url, popup_image_url, null, titles) + ' </a>';
+  var url = '/cached_finds/' + $ifloat_body.find_id + '/products_for/' + checksum;
+  var tally = '<div class="tally">' + count + ' item' + (count > 1 ? 's' : '') + '</div>';
+  return '<a class="product" href="' + url + '"> ' + tally + product_image_make(image_url, popup_image_url, null, titles) + ' </a>';
 }
 
 function find_results_update() {
-	var url = '/cached_finds/' + $ifloat_body.find_id + '/images'
-	$.getJSON(url, find_results_update_handle);
+  var url = '/cached_finds/' + $ifloat_body.find_id + '/images'
+  $.getJSON(url, find_results_update_handle);
 }
 
 function find_results_update_handle(data) {
-	var filtered_prod_count = data.shift();
-	var image_count = data.length;
-	
-	var frps = $('#find_results_products');
-	var insertion_point = frps.find('.terminator');
-	
-	var products = [];
-	for(var i in data) {
-		var d = data[i];
-		products.push(find_results_make(d[0], d[1], d[2], d[3], d[4]));
-	}
-	
-	frps.find('p').remove();
-	frps.find('.product').remove();
-	frps.find('.terminator').before(products.join(' '));
-	
-	var remaining = (filtered_prod_count == $ifloat_body.find_total ? '' : ' - ' + util_pluralize(filtered_prod_count, ' remain', true) + ' after filtering');
-	$('#find_results_remaining').text(remaining);
-	
-	if(data.length == 0) {
-		frps.find('.terminator').before('<p>You have filtered out all possible products. To fix this, <strong>relax or remove some of your filters</strong>.</p>');
-	}
+  var filtered_prod_count = data.shift();
+  
+  var frps = $('#find_results_products');
+  var insertion_point = frps.find('.terminator');
+  
+  var displayed_count = 0;
+  var products = [];
+  for(var i in data) {
+    var d = data[i];
+    products.push(find_results_make(d[0], d[1], d[2], d[3], d[4]));
+    displayed_count += d[1];
+  }
+  
+  frps.find('p').remove();
+  frps.find('.product').remove();
+  
+  var terminator = frps.find('.terminator');
+  if($ifloat_body.prompt_to_add_filter && filtered_prod_count > displayed_count) {
+    terminator.before('<p class="add">&larr; <strong>add a filter</strong> to narrow your results</p>');
+    frps.find('p').animate({marginLeft: '10px', opacity: 1}).animate({color: 'yellow'}).animate({color: 'black'});
+    $ifloat_body.prompt_to_add_filter = false;
+  }
+  terminator.before(products.join(' '));
+  
+  var remaining = (filtered_prod_count == $ifloat_body.find_total ? '' : ' - ' + util_pluralize(filtered_prod_count, ' remain', true) + ' after filtering');
+  $('#find_results_remaining').text(remaining);
+  
+  if(data.length == 0) {
+    frps.find('.terminator').before('<p class="relax">You have filtered out all possible products. To fix this, <strong>relax or remove some of your filters</strong>.</p>');
+  }
 }

306d2e21de5ff7b8a8efcb994adfd29d90eccb52
Andre Ben Hamou <andre@bluetheta.com>
2011-01-03 12:07:55 +0000

    The entire login form is now generated clientside.

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -13,8 +13,43 @@ function login_logout() {
 
 function login_open(message) {
   var login_register = $('#login_register');
-  message = (message ? message : '');
-  login_register.find('.message').text(message);
+  
+  if(login_register.length == 0) {
+    var html = ['<div id="login_register" title="Login / Register">'];
+    html.push('<p class="message"> </p>');
+    
+    html.push('<form class="login" action="/users/login" method="post" onsubmit="return login_submit(event)">');
+    html.push('<h3>Login</h3>');
+    html.push('<p> <label>E-mail: <input type="text" name="login" /></label> </p>');
+    html.push('<p> <label>Password: <input type="password" name="password" /></label> </p>');
+    html.push('<p class="submit">');
+    html.push('<input type="submit" value="Login" onclick="login_operation(\'login\')" name="form_submit" id="login_submit_button" />');
+    html.push('<input type="submit" value="Reset Password" onclick="login_operation(\'reset\')" name="form_submit" id="reset_password_submit" />');
+    html.push('</p>');
+    html.push('</form>');
+    
+    html.push('<form class="register" action="/users" method="post" onsubmit="return login_submit(event)">');
+    html.push('<h3>Register</h3>');
+    html.push('<p> <label>Name: <input type="text" name="name" /></label> </p>');
+    html.push('<p> <label>Nickname: <input type="text" name="nickname" /></label> </p>');
+    html.push('<p> <label>E-mail: <input type="text" name="login" /></label> </p>');
+    html.push('<p> <label>Password: <input type="password" name="password" /></label> </p>');
+    html.push('<p> <label>Confirmation: <input type="password" name="confirmation" /></label> </p>');
+    html.push('<p class="marketing">From time to time, we like to involve our users in marketing activities that make suppliers and retailers more effective for their customers. Please tick here <input type="checkbox" name="send_marketing" /> if you would like to participate in these activities.</p>');
+    html.push('<p class="submit">');
+    html.push('<input type="submit" value="Register" onclick="login_operation(\'register\')" name="form_submit" id="register_submit" />');
+    html.push('</p>');
+    html.push('</form>');
+    
+    html.push('</div>');
+    
+    $('body').append(html.join(' '));
+    
+    login_register = $('#login_register');
+    login_register.dialog({autoOpen: false, close: login_reset, resizable: false, width: '700px'});
+  }
+  
+  login_register.find('.message').text(message ? message : '');
   login_register.dialog('open');
 }
 

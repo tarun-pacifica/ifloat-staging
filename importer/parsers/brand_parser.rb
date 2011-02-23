b861bb26f4b02a676b85d8d64b9590424ba405b8
Andre Ben Hamou <andre@bluetheta.com>
2011-02-23 23:43:32 +0000

    Added Brand parser to new importer.

---
+++ importer/parsers/brand_parser.rb
@@ -0,0 +1,20 @@
+class BrandParser < AbstractParser
+  HEADERS = REQUIRED_VALUE_HEADERS = %w(company.reference marketing:brand logo)
+    
+  def generate_objects(parsed_fields)
+    attributes = [:asset, :company, :name].zip(parsed_fields.values_at(*HEADERS))
+    [Hash[attributes].update(:class => Brand)]
+  end
+  
+  def parse_field(head, value, fields)
+    case head
+    when "company.reference" then lookup!(Company, value)
+    when "logo" then lookup!(Asset, "brand_logos", fields["company.reference"], value)
+    else value
+    end
+  end
+  
+  def partition_fields(values_by_header)
+    values_by_header.group_by { |header, value| header == "company.reference" ? 0 : 1 }.sort.map { |i, fields| fields }
+  end
+end

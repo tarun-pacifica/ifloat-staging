5914ed3da793e37db02b5383d0062c483d4941b2
Andre Ben Hamou <andre@bluetheta.com>
2011-03-14 23:47:01 +0000

    Began implementing the basket panel population logic.

--- public/javascripts/basket_panel.js
+++ public/javascripts/basket_panel.js
@@ -2,41 +2,33 @@ function basket_panel_load() {
   $.getJSON('/picked_products', basket_panel_load_handle);
 }
 
-function basket_panel_load_handle(data) {
-  console.log(data);
+function basket_panel_load_handle(picks_by_group) {
+  var html = [].concat(
+    basket_panel_load_handle_buy_now(picks_by_group.buy_now),
+    basket_panel_load_handle_buy_later(picks_by_group.buy_later),
+    basket_panel_load_handle_compare(picks_by_group.compare)
+  );
+  
+  $('#basket_panel').append(html.join(' '));
 }
 
-// function product_detail_pick_button_click(event) {
-//   var button = util_target(event);
-//   var to_group = null;
-//   var groups = ['buy_later', 'buy_now', 'compare'];
-//   for(var i in groups) {
-//     var g = groups[i];
-//     if(button.hasClass(g)) {
-//       to_group = g;
-//       break;
-//     }
-//   }
-//   
-//   var data = button.parent().data();
-//   var from_group = data.group;
-//   var pick_id = data.pick_id;
-//   
-//   if(to_group == null) {
-//     if(from_group) pick_list_remove(from_group, pick_id);
-//   }
-//   else if(from_group) pick_list_move(from_group, to_group, pick_id);
-//   else pick_list_add(to_group, $ifloat_body.product_id);
-// }
-// 
-// function product_detail_pick_buttons_update(group, pick_id) {
-//   var pick_buttons = $('#pick_buttons');
-//   if(pick_buttons.length == 0) return;
-//   
-//   pick_buttons.removeData();
-//   if(group) pick_buttons.data('group', group)
-//   if(pick_id) pick_buttons.data('pick_id', pick_id);
-//   
-//   pick_buttons.find('div').removeClass('selected').unbind('click').click(product_detail_pick_button_click);
-//   if(group) pick_buttons.find('.' + group).addClass('selected').unbind('click');
-// }
+function basket_panel_load_handle_buy_now(picks) {
+  if (!picks) return [];
+  
+  var html = [];
+  return html
+}
+
+function basket_panel_load_handle_buy_later(picks) {
+  if (!picks) return [];
+  
+  var html = [];
+  return html
+}
+
+function basket_panel_load_handle_compare(picks) {
+  if (!picks) return [];
+  
+  var html = [];
+  return html
+}

6f831a78236e190f8a2cd0924c32ae3ceeff347d
Andre Ben Hamou <andre@bluetheta.com>
2011-03-20 15:48:25 +0000

    Finished wiring up variant selector.

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -23,9 +23,10 @@ function product_property_section_select(event) {
 
 var product_sibling_prod_ids_by_value_by_prop_ids = {};
 function product_sibling_select() {
-  var prod_id_intersection = null;
+  var siblings = $('#pick_sibling .sibling');
   
-  $('#pick_sibling .sibling').each(function () {
+  var prod_id_intersection = null;
+  siblings.each(function () {
     var prop_id = $(this).attr('id').split('_')[2];
     var value = $(this).find('select').val();
     
@@ -47,8 +48,46 @@ function product_sibling_select() {
   var prod_ids = [];
   for(prod_id in prod_id_intersection) prod_ids.push(prod_id);
   
-  if(prod_ids.length == 1) window.location = '/products/sibling-' + prod_ids[0];
-  // TODO: hide obsolete selects
+  var selects = siblings.find('select')
+  selects.removeAttr('disabled');
+  selects.find('option').removeAttr('disabled');
+  
+  if(prod_ids.length == 0) return;
+
+  if(prod_ids.length == 1) {
+    window.location = '/products/sibling-' + prod_ids[0];
+    return;
+  }
+  
+  for(var prop_id in product_sibling_prod_ids_by_value_by_prop_ids) {
+    var prod_ids_by_value = product_sibling_prod_ids_by_value_by_prop_ids[prop_id];
+    var disabled_values_exist = false, enabled_values = [];
+    
+    for(var value in prod_ids_by_value) {
+      var enabled = false;
+      var prod_ids = prod_ids_by_value[value];
+      for(var i in prod_ids) enabled = (enabled || prod_id_intersection[prod_ids[i]]);
+      if(enabled) enabled_values.push(value);
+      else disabled_values_exist = true;
+    }
+    
+    if(!disabled_values_exist) continue;
+        
+    var select = $('#sibling_property_' + prop_id + ' select');
+    var options = select.find('option');
+    
+    if(enabled_values.length == 0) {
+      select.attr('disabled', 'disabled');
+      continue;
+    }
+    
+    var enabled_values = util_hash_from_array(enabled_values, true);
+    
+    options.each(function(index) {
+      var option = $(this);
+      if(index > 0 && !enabled_values[option.val()]) option.attr('disabled', 'disabled');
+    });
+  }
 }
 
 function product_siblings_wire_up(prod_ids_by_value_by_prop_ids) {

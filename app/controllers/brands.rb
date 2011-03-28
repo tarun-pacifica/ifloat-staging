3b46b6c739eb6f4f3bc14af05b19a638b97263ef
Andre Ben Hamou <andre@bluetheta.com>
2011-03-28 10:06:23 +0100

    Styling / feature changes to brand page.

--- app/controllers/brands.rb
+++ app/controllers/brands.rb
@@ -4,6 +4,9 @@ class Brands < Application
     @brand = Brand.first(:name => name)
     return categories_404 if @brand.nil?
     
+    @canonical_path = "/brands/#{URI.escape(@brand.name)}"
+    return redirect(@canonical_path, :status => 301) unless @canonical_path == request.path
+    
     product_ids_by_node = @brand.product_ids_by_category_node([])
     return categories_404 if product_ids_by_node.empty?
     
@@ -12,10 +15,10 @@ class Brands < Application
     @show_all_links_by_node = {}
     product_ids_by_node.keys.each do |node|
       brand_filter = [Indexer.brand_property_id, "ENG", name, nil]
-      @show_all_links_by_node[node] = category_link(node, "show all", false, [brand_filter])
+      count = @product_links_by_node[node].size
+      @show_all_links_by_node[node] = category_link(node, "show all #{count}", false, [brand_filter])
     end
     
-    @brand_url = @canonical_path = "/brands/#{URI.escape(name)}"
     @page_description = @brand.description
     @page_title = name
     render

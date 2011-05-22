dc6900937cc3e090cbd55122a1022f62ae6275ea
Andre Ben Hamou <andre@bluetheta.com>
2011-05-22 17:38:19 +0100

    Fixed / completed TextPropertyValue spec.

--- app/models/text_property_value.rb
+++ app/models/text_property_value.rb
@@ -10,18 +10,17 @@ class TextPropertyValue < PropertyValue
   
   validates_presence_of :language_code, :text_value
   
-  # TODO: spec
   def self.parse_or_error(value)
     marked_value = value.gsub(/[^\n\w\.\/\- !@%()'";:,?®™#]/) { |c| " >>> #{c} <<< " }
     raise "invalid characters: #{marked_value}" if marked_value.size > value.size 
     {:text_value => value}
   end
   
-  def comparison_key # TODO: spec
+  def comparison_key
     [text_value]
   end
   
-  def to_s(range_sep = nil) # TODO: spec
+  def to_s(range_sep = nil)
     text_value
   end
 end
--- spec/models/text_property_value_spec.rb
+++ spec/models/text_property_value_spec.rb
@@ -1,28 +1,16 @@
 require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe TextPropertyValue do
-
-  before(:all) do
-    @text = PropertyType.create(:core_type => "text", :name => "text")
-  end
-  
-  after(:all) do
-    @text.destroy
-  end
-  
-  def mock_text_value(value)
-    t = TextPropertyValue.new(:product_id => 1, :property_definition_id => 1, :value => value, :language_code => "ENG")
-    t.stub!(:property_type).and_return(@text) # is this still needed?
-    t
-  end
-  
   describe "creation" do
     before(:each) do
-      @value = mock_text_value("red")
+      @value = TextPropertyValue.new(:product_id => 1, :property_definition_id => 1, :sequence_number => 1,
+                                     :text_value => "text", :language_code => "ENG", :auto_generated => 1)
     end
     
     it "should succeed with valid data" do
       @value.should be_valid
+      @value.comparison_key.should == ["text"]
+      @value.to_s.should == "text"
     end
     
     it "should fail without a product" do
@@ -54,70 +42,9 @@ describe TextPropertyValue do
     end
   end
   
-  describe "creation with existing value (of the same language code) for a given property definition and product" do    
-    before(:all) do
-      @value = mock_text_value("red")
-      @value.save
-    end
-    
-    after(:all) do
-      @value.destroy
-    end
-    
-    it "should succeed with a different value" do
-      mock_text_value("green").should be_valid
-    end
-    
-    it "should fail with the same value" do
-      mock_text_value("red").should_not be_valid
-    end
-  end
-  
-  describe "finding product IDs matching a given spec" do
-    before(:all) do
-      @properties = {
-        "brand"   => @text.definitions.create(:name => "marketing:brand", :findable => true, :sequence_number => 1),
-        "colour"  => @text.definitions.create(:name => "appearance:colour", :findable => true, :sequence_number => 2),
-        "pattern" => @text.definitions.create(:name => "appearance:pattern", :findable => true, :sequence_number => 3)
-      }
-
-      @products = [
-        {"brand" => ["Ford",  "ENG"], "colour" => ["Red", "ENG"]},
-        {"brand" => ["Ford",  "ENG"], "colour" => ["White", "ENG"]},
-        {"brand" => ["Audi",  "ENG"], "colour" => ["Red", "ENG"], "pattern" => ["Forded", "ENG"]},
-        {"brand" => ["Clio",  "FRA"], "colour" => ["Rouge", "FRA"]},
-        {"brand" => ["Rouge", "ENG"], "colour" => ["Puce", "ENG"]}
-      ].each_with_index.map do |info, i|
-        product = Product.create(:company_id => i, :reference => "AF11235")
-        info.each do |key, info|
-          TextPropertyValue.create(:product => product, :definition => @properties[key],
-                                   :value => info[0], :language_code => info[1])
-        end
-        product
-      end
-    end
-    
-    after(:all) do
-      @properties.each { |key, property| property.destroy }
-      @products.each do |product|
-        product.values.destroy!
-        product.destroy
-      end
-    end
-    
-    [ ["Ford",           ["ENG"],        3],
-      ["FORD",           ["ENG"],        3],
-      ["Forded",         ["ENG"],        1],
-      ["Red",            ["ENG"],        2],
-      ["Audi",           ["ENG"],        1],
-      ["Clio",           ["FRA"],        1],
-      ["Clio",           ["DEU"],        0],
-      ["Rouge",          ["ENG", "FRA"], 2],
-      ["Audi Ford",      ["ENG"],        1]
-    ].each do |spec, languages, expected_count|
-      it "should return #{expected_count} IDs for #{spec.inspect} #{languages.inspect}" do
-        TextPropertyValue.product_ids_matching_spec(spec, languages).size.should == expected_count
-      end
+  describe "parsing" do
+    it "fail and mark invalid characters" do
+      proc { TextPropertyValue.parse_or_error("abc]def") }.should raise_error("invalid characters: abc >>> ] <<< def")
     end
   end
 end

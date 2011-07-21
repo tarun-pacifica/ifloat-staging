4e36c35e2c277d731c0bcd428d125cfc6ae8d938
Andre Ben Hamou <andre@bluetheta.com>
2011-07-21 13:12:42 +0100

    DatePropertyValues now parse to a BigNum as expected.

--- app/models/date_property_value.rb
+++ app/models/date_property_value.rb
@@ -12,17 +12,17 @@ class DatePropertyValue < NumericPropertyValue
   
   def self.format_value(value, params = {})
     v = value.to_i
-    ymd = [v / 10000, (v / 100) % 100, v % 100].select { |n| n > 0 }    
+    ymd = [v / 10000, (v / 100) % 100, v % 100].select { |n| n > 0 }
     format = FORMATS[params[:verbose] ? :verbose : :compact][ymd.size - 1]
     Date.new(*ymd).strftime(format)
   end
   
-    
+  
   protected
   
   def self.parse_atom(atom)
     raise "invalid format (expected YYYYMMDD) in #{atom.inspect}" unless atom.to_s =~ /^(\d{4})(\d\d)(\d\d)$/
     raise "invalid format (expected YYYY0000) in #{atom.inspect}" if $2.to_i == 0 and $3.to_i > 0
-    atom.to_i
+    super(atom)
   end
 end

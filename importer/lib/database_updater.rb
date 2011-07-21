76b50204a1a61d1b8e9f4438a5299dfc6bc251d7
Andre Ben Hamou <andre@bluetheta.com>
2011-07-21 20:43:57 +0100

    More work on the new importer's database updater.

--- importer/lib/database_updater.rb
+++ importer/lib/database_updater.rb
@@ -8,34 +8,62 @@ class DatabaseUpdater
     @objects = object_catalogue
     
     @adapter = DataMapper.repository(:default).adapter
-    @db_ids_by_pk_md5 = {} # TODO: consider making this an OM hash table (conserve mem) - test with a full insert set
-    @db_value_md5s_by_pk_md5 = {} # ditto
     @errors = []
   end
   
-  def gather_db_keys
-    @classes.each do |klass|
-      pks_vks = ObjectRef.keys_for(klass)
-      next if pks_vks.first.nil?
-      
-      child_keys_by_rel_name = Hash[klass.relationships.map { |name, rel| [name.to_sym, rel.child_key.first.name] }]
-      pk_fields, value_fields = pks_vks.map do |keys|
-        keys.map do |key|
-          key = (child_keys_by_rel_name[key] || key)
-          "IFNULL(#{key}, '')"
-        end.join(",'::',")
+  def scan_db(klass)
+    query = <<-SQL
+      CREATE TEMPORARY TABLE IF NOT EXISTS #{klass.storage_name}_md5s (
+        id INT(10) unsigned PRIMARY KEY,
+        pk_md5 VARCHAR(32) NOT NULL,
+        value_md5 VARCHAR(32)
+      ) ENGINE=MEMORY DEFAULT CHARSET=utf8
+    SQL
+    @adapter.execute(query)
+    
+    pks_vks = ObjectRef.keys_for(klass)
+    return if pks_vks.first.nil?
+    
+    relationships = klass.relationships
+    pk_fields, value_fields = pks_vks.each_with_index.map do |keys, i|
+      components = keys.map do |key|
+        rel = relationships[key]
+        field =
+          if rel.nil? then "t1.#{key}"
+          else "(SELECT pk_md5 FROM #{rel.parent_model.storage_name}_md5s WHERE id = t1.#{rel.child_key.first.name})"
+          end
+        "IFNULL(#{field}, '')"
       end
       
-      query = "SELECT id, MD5(CONCAT(#{pk_fields})) AS pk_md5"
-      query += (value_fields.empty? ? ", NULL AS value_md5" : ", MD5(CONCAT(#{value_fields})) AS value_md5")
-      query += " FROM #{klass.storage_name}"
-      query += " WHERE type = '#{klass}'" if klass.properties.named?(:type)
+      components.unshift("'#{klass}'") if i == 0
+      components.empty? ? "MD5('')" : "MD5(CONCAT(#{components.join(",'::',")}))"
+    end
+    
+    query = <<-SQL
+      INSERT INTO #{klass.storage_name}_md5s
+      SELECT id, #{pk_fields} AS pk_md5, #{value_fields} AS value_md5
+      FROM #{klass.storage_name} t1
+    SQL
+    query += "WHERE type = '#{klass}'" if klass.properties.named?(:type)
+    @adapter.execute(query) # TODO: cope explicitly with table full errors
+    
+    valid_pk_md5s_seen = []
+    query = "SELECT * FROM #{klass.storage_name}_md5s"
+    query += " WHERE id IN (SELECT id FROM #{klass.storage_name} WHERE type = '#{klass}')" if klass.properties.named?(:type)
+    @adapter.select(query).each do |record|
+      id, pk_md5, db_value_md5 = record.id, record.pk_md5, record.value_md5
       
-      @adapter.select(query).each do |record|
-        @db_ids_by_pk_md5[record.pk_md5] = record.id
-        @db_value_md5s_by_pk_md5[record.pk_md5] = record.value_md5
+      value_md5 = @objects.value_md5_for(pk_md5)
+      if value_md5.nil?
+        puts "delete #{pk_md5} (#{id})"
+        break
+      elsif value_md5 != db_value_md5
+        puts "update #{pk_md5} (#{id}): #{db_value_md5} -> #{value_md5}"
+        valid_pk_md5s_seen << pk_md5
+        break
       end
     end
+    valid_pk_md5s_seen
   end
   
   def update
@@ -44,7 +72,9 @@ class DatabaseUpdater
       transaction.begin
       @adapter.push_transaction(transaction)
       
-      gather_db_keys
+      @adapter.execute("SET max_heap_table_size = 256*1024*1024")
+      valid_pk_md5s_seen = @classes.map(&method(:scan_db)).flatten.to_set
+      # do inserts
       
       # keys = @objects.keys
       # begin tran

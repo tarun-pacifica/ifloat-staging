990d589db99500f8b58831ef7f39b72fdbbb1def
Andre Ben Hamou <andre@bluetheta.com>
2011-11-24 17:24:52 +0000

    Updated default page description.

--- app/controllers/categories.rb
+++ app/controllers/categories.rb
@@ -51,7 +51,7 @@ class Categories < Application
     @canonical_path = ["/categories", root, sub].compact.join("/")
     @page_title = @path_names.join(" - ") unless @path_names.empty?
     @page_description = Indexer.category_definition_for_node(@path_names)
-    @page_description ||= "ifloat® is an innovative boating, sailing, sailboating and yachting, marine equipment and products website. The site has exceptionally powerful filter tools to find or search for products from the comprehensive ifloat® on-site database.  All products for sale from our partner retailer or chandery are in stock/ex-stock and ready to buy now. Many products are at a discount on the recommended retail price/RRP giving some of the best prices for boating products on the web." if @path_names.empty?
+    @page_description ||= "Find and buy boat equipment, accessories, sailing clothing and sea books and charts." if @path_names.empty?
     render
   end
   

187fdaa956c31256a5e6a3ef01b370897ace29ba
Andre Ben Hamou <andre@bluetheta.com>
2011-12-19 11:03:07 +0000

    Increased the number of supported units in PropertyType parser to 10.

--- importer/parsers/property_type_parser.rb
+++ importer/parsers/property_type_parser.rb
@@ -1,10 +1,11 @@
 class PropertyTypeParser < AbstractParser
-  REQUIRED_HEADERS = ["property type", "raw type", "unit-1", "unit-2", "unit-3", "unit-4", "unit-5"]
+  UNIT_HEADERS = 10.times.map { |i| "unit-#{i + 1}" }
+  REQUIRED_HEADERS = ["property type", "raw type"] + UNIT_HEADERS
   REQUIRED_VALUE_HEADERS = ["property type", "raw type"]
   
   def generate_objects(parsed_fields)
     name, core_type = parsed_fields.values_at("property type", "raw type")
-    units = parsed_fields.values_at("unit-1", "unit-2", "unit-3", "unit-4", "unit-5").compact
+    units = parsed_fields.values_at(*UNIT_HEADERS).compact
     [{:class => PropertyType, :name => name, :core_type => core_type, :units => units}]
   end
 end
